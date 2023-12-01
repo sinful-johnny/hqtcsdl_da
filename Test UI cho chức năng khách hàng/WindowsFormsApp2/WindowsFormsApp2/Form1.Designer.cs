a0e1fe190bf2dca1e35fcfceda23b2437546d992
sinful_johnny <megaVNN321123@gmail.com>
2023-12-02 02:44:38 +0700

    commit my depression

--- Test UI cho chức năng khách hàng/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ Test UI cho chức năng khách hàng/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.dsKH = new System.Windows.Forms.ListBox();
             this.username = new System.Windows.Forms.TextBox();
             this.password = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.ttcanhan = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // button1
@@ -44,16 +44,6 @@
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // dsKH
-            // 
-            this.dsKH.FormattingEnabled = true;
-            this.dsKH.ItemHeight = 16;
-            this.dsKH.Location = new System.Drawing.Point(12, 124);
-            this.dsKH.Name = "dsKH";
-            this.dsKH.Size = new System.Drawing.Size(776, 36);
-            this.dsKH.TabIndex = 2;
-            this.dsKH.SelectedIndexChanged += new System.EventHandler(this.dsKH_SelectedIndexChanged_1);
             // 
             // username
             // 
@@ -84,18 +74,28 @@
             this.label1.TabIndex = 5;
             this.label1.Text = "Info";
             // 
+            // ttcanhan
+            // 
+            this.ttcanhan.HideSelection = false;
+            this.ttcanhan.Location = new System.Drawing.Point(12, 121);
+            this.ttcanhan.Name = "ttcanhan";
+            this.ttcanhan.Size = new System.Drawing.Size(776, 64);
+            this.ttcanhan.TabIndex = 6;
+            this.ttcanhan.UseCompatibleStateImageBehavior = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ttcanhan);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.password);
             this.Controls.Add(this.username);
-            this.Controls.Add(this.dsKH);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "SQL Database demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,10 +103,10 @@
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox dsKH;
         private System.Windows.Forms.TextBox username;
         private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView ttcanhan;
     }
 }
 
--- Test UI cho chức năng khách hàng/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ Test UI cho chức năng khách hàng/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp2
 {
@@ -17,15 +18,32 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
 
-            UpdateDsKH();
+            //UpdateDsKH();
         }
 
-        private void UpdateDsKH()
+        //private void UpdateDsKH()
+        //{
+        //    dsKH.DataSource = data;
+        //    dsKH.DisplayMember = "Info";
+        //}
+        private void UpdateListView()
         {
-            dsKH.DataSource = data;
-            dsKH.DisplayMember = "Info";
-        }
+            //ListViewItem newItem = new ListViewItem("sth");
+            //newItem.SubItems.Add("sth1");
+            //newItem.SubItems.Add("sth2");
+            //newItem.SubItems.Add("sth3");
+            //ttcanhan.Items.Add(newItem);
+            foreach(TAI_KHOAN tAI_KHOAN in data)
+            {
+                ListViewItem newItem = new ListViewItem(tAI_KHOAN.ID_TAIKHOAN);
+                newItem.SubItems.Add(tAI_KHOAN.SDT);
+                newItem.SubItems.Add(tAI_KHOAN.MATKHAU);
+                newItem.SubItems.Add(tAI_KHOAN.LOAITK);
+                ttcanhan.Items.Add(newItem);
+            }
 
+        }
+        
         private void dsKH_SelectedIndexChanged(object sender, EventArgs e)
         {
             return;
@@ -36,28 +54,23 @@ namespace WindowsFormsApp2
             DatabaseAccess db = new DatabaseAccess();
             //data = db.GetTAI_KHOANs();
             data = db.loginDB(username.Text, password.Text);
-            UpdateDsKH();
+            //UpdateDsKH();
+            UpdateListView();
             //Helper helper = new Helper("DESKTOP-CDH2DEU\\SQLSERVER", "QL_NHAKHOA", "sa", "123");
             //MessageBox.Show(helper.connectionString);
         }
         int count1 = 0;
-        private void dsKH_SelectedIndexChanged_1(object sender, EventArgs e)
-        {
-            if(count1 == 0)
-            {
-                count1 = 1;
-            }
-            else
-            {
-                MessageBox.Show(dsKH.Text.Split('\t')[0]);
-            }
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
+        //private void dsKH_SelectedIndexChanged_1(object sender, EventArgs e)
+        //{
+        //    if(count1 == 0)
+        //    {
+        //        count1 = 1;
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show(dsKH.Text.Split('\t')[0]);
+        //    }
+        //}
         private void Username_Leave(object sender, EventArgs e)
         {
             if(username.Text == "")
@@ -87,6 +100,15 @@ namespace WindowsFormsApp2
             {
                 password.Text = "";
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ttcanhan.View = View.Details;
+            ttcanhan.Columns.Add("ID_TAIKHOAN", 100, HorizontalAlignment.Left);
+            ttcanhan.Columns.Add("SDT", 100);
+            ttcanhan.Columns.Add("MAT KHAU", 100);
+            ttcanhan.Columns.Add("LOAI TAI KHOAN", 100);
         }
     }
 }
