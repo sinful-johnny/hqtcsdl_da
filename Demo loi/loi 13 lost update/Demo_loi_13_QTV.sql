cc1ebb7978aec3289f7e1f30bdaf6e9e8dd2fe09
sinful_johnny <megaVNN321123@gmail.com>
2024-01-06 22:11:48 +0700

    revert false commit

---
+++ Demo loi/loi 13 lost update/Demo_loi_13_QTV.sql
@@ -0,0 +1,92 @@
+﻿-- Code Proc bị lỗi
+CREATE OR ALTER PROC sp_CAPNHAT_TAIKHOAN_NGUOIDUNG
+	@idtaikhoan VARCHAR(255),
+	@tentaikhoan NVARCHAR(30) = NULL,
+	@ngaysinh DATE = NULL,
+	@email VARCHAR(30) = NULL,
+	@loaitaikhoan varchar(20) = NULL
+AS
+
+BEGIN TRAN
+	DECLARE @OLDloaitaikhoan varchar(20)
+	SET @OLDloaitaikhoan = (SELECT LOAITK FROM V_THEM_XOA_SUA_TK WHERE ID_TAIKHOAN = @idtaikhoan)
+	IF (@OLDloaitaikhoan = '')
+		BEGIN
+			RAISERROR (N'Không tồn tại id tài khoản. Vui lòng thử lại', 0, 0)
+			ROLLBACK TRAN
+			RETURN
+		END
+
+	IF (LEN(ISNULL(@tentaikhoan, '')) = 0)
+		BEGIN
+			SET @tentaikhoan = (SELECT HOTEN FROM TAI_KHOAN WHERE ID_TAIKHOAN = @idtaikhoan)
+		END
+
+
+	IF (LEN(ISNULL(@ngaysinh, '')) = 0)
+		BEGIN
+			SET @ngaysinh = (SELECT NGAYSINH FROM TAI_KHOAN WHERE ID_TAIKHOAN = @idtaikhoan)
+		END
+
+
+	IF (LEN(ISNULL(@email, '')) = 0)
+		BEGIN
+			SET @email = (SELECT EMAIL FROM TAI_KHOAN WHERE ID_TAIKHOAN = @idtaikhoan)
+		END
+
+
+	IF (LEN(ISNULL(@loaitaikhoan, '')) = 0)
+		BEGIN
+			SET @loaitaikhoan = (SELECT LOAITK FROM TAI_KHOAN WHERE ID_TAIKHOAN = @idtaikhoan)
+		END
+	ELSE IF (@loaitaikhoan = 'QTV')
+		BEGIN
+			RAISERROR (N'Không thể phân thành quản trị viên. Vui lòng thử lại', 0, 0)
+			ROLLBACK TRAN
+			RETURN
+		END
+
+
+	UPDATE V_THEM_XOA_SUA_TK
+	SET HOTEN = @tentaikhoan,
+		NGAYSINH = @ngaysinh,
+		EMAIL = @email,
+		LOAITK = @loaitaikhoan
+	WHERE ID_TAIKHOAN = @idtaikhoan
+			
+	IF (@@ERROR <> 0)
+		BEGIN
+			RAISERROR (N'Không thể cập nhật. Vui lòng thử lại', 0, 0)
+			ROLLBACK TRAN
+			RETURN
+		END
+COMMIT TRAN
+GO
+
+--Demo lỗi---------------------------------------------------
+declare @idtaikhoan VARCHAR(255),
+	@tentaikhoan NVARCHAR(30) = NULL,
+	@ngaysinh DATE = NULL,
+	@email VARCHAR(30) = NULL,
+	@loaitaikhoan varchar(20) = NULL
+
+select @idtaikhoan = ID_TAIKHOAN
+from TAI_KHOAN
+where SDT = '0911122334'
+
+set @ngaysinh = '2022-1-1'
+set @email = 'sieudeptrai@game.com'
+
+exec sp_CAPNHAT_TAIKHOAN_NGUOIDUNG @idtaikhoan, @tentaikhoan, @ngaysinh, @email, @loaitaikhoan
+go
+select * from TAI_KHOAN where SDT = '0911122334'
+
+SELECT CASE transaction_isolation_level 
+    WHEN 0 THEN 'Unspecified' 
+    WHEN 1 THEN 'ReadUncommitted' 
+    WHEN 2 THEN 'ReadCommitted' 
+    WHEN 3 THEN 'Repeatable' 
+    WHEN 4 THEN 'Serializable' 
+    WHEN 5 THEN 'Snapshot' END AS TRANSACTION_ISOLATION_LEVEL 
+FROM sys.dm_exec_sessions 
+where session_id = @@SPID
